050b572c0ce0617d10ca99b40b7dfb96f58c590f
Kaik Oliveira <kaik.oliveiraa76@gmail.com>
2022-08-19 16:35:17 -0300

    feat: fist version button ripple

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,78 @@
 import * as React from "react";
 
-import { Container } from "./styles";
+import * as S from "./styles";
 
-export const Button: React.FC = () => {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  isLoading?: boolean;
+  maxWidth?: string;
+  height?: string;
+  backgroundColor?: string;
+  colorLoading?: string;
+}
+
+const ButtonBase: React.ForwardRefRenderFunction<
+  HTMLButtonElement,
+  ButtonProps
+> = (
+  {
+    children,
+    onClick,
+    backgroundColor = "#08090A",
+    colorLoading = "#fff",
+    isLoading = false,
+    maxWidth,
+    height,
+    ...rest
+  }: ButtonProps,
+  refBtn
+) => {
+  const buttonRef = React.useRef<HTMLButtonElement>(null);
+
+  const [coords, setCoords] = React.useState({ x: -1, y: -1 });
+  const [isRippling, setIsRippling] = React.useState(false);
+
+  const ref = refBtn || buttonRef;
+
+  React.useEffect(() => {
+    if (coords.x !== -1 && coords.y !== -1) {
+      setIsRippling(true);
+      setTimeout(() => setIsRippling(false), 300);
+    } else setIsRippling(false);
+  }, [coords]);
+
+  React.useEffect(() => {
+    if (!isRippling) setCoords({ x: -1, y: -1 });
+  }, [isRippling]);
+
   return (
-    <Container>
-      <h1>fist button</h1>
-    </Container>
+    <S.Container
+      backgroundColor={backgroundColor}
+      onClick={(e: any) => {
+        const rect = e.target.getBoundingClientRect();
+        setCoords({ x: e.clientX - rect.left, y: e.clientY - rect.top });
+        onClick && onClick(e);
+      }}
+      disabled={isLoading || rest.disabled}
+      isLoading={isLoading}
+      maxWidth={maxWidth}
+      maxHeight={height}
+      ref={ref}
+      {...rest}
+    >
+      {isRippling && (
+        <S.Ripple
+          backgroundColor={backgroundColor}
+          left={coords.x}
+          top={coords.y}
+        />
+      )}
+      <S.LoadingComponent color={colorLoading} isShow={isLoading}>
+        <span>loading</span>
+      </S.LoadingComponent>
+      <S.Content>{children}</S.Content>
+    </S.Container>
   );
 };
+
+export const Button = React.forwardRef(ButtonBase);
--- src/styles.ts
+++ src/styles.ts
@@ -1,5 +1,125 @@
-import styled from "styled-components";
+import { lighten, shade } from "polished";
+import styled, { css } from "styled-components";
 
-export const Container = styled.div`
-  background-color: red;
+interface RippleProps {
+  backgroundColor: string;
+  left: number;
+  top: number;
+}
+
+interface ButtonContainerProps {
+  isLoading: boolean;
+  backgroundColor: string;
+  maxWidth?: string;
+  maxHeight?: string;
+}
+
+type LoadingComponentProps = {
+  isShow: boolean;
+  color: string;
+};
+
+export const Container = styled.button<ButtonContainerProps>`
+  border-radius: 0.313rem;
+  border: none;
+  padding: 0.713rem 1rem;
+  overflow: hidden;
+  position: relative;
+  cursor: pointer;
+  transition: all 0.3s ease-in;
+  width: 100%;
+  height: 100%;
+
+  ${({ backgroundColor }) => css`
+    background-color: ${backgroundColor};
+
+    &:hover {
+      background-color: ${shade(0.08, backgroundColor)};
+    }
+  `};
+
+  ${({ maxWidth }) =>
+    maxWidth &&
+    css`
+      max-width: ${maxWidth};
+    `}
+
+  ${({ maxHeight }) =>
+    maxHeight &&
+    css`
+      max-height: ${maxHeight};
+    `}
+
+  ${({ isLoading }) =>
+    isLoading &&
+    css`
+      color: transparent;
+    `}
+
+  &:disabled {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+`;
+
+export const Ripple = styled.span<RippleProps>`
+  width: 20px;
+  height: 20px;
+  position: absolute;
+  background: ${({ backgroundColor }) => lighten(0.06, backgroundColor)};
+  left: ${({ left }) => left}px;
+  top: ${({ top }) => top}px;
+  display: block;
+  content: "";
+  border-radius: 9999px;
+  opacity: 1;
+  animation: 1.5s ease 1 forwards ripple-effect;
+
+  @keyframes ripple-effect {
+    0% {
+      transform: scale(1);
+      opacity: 1;
+    }
+    50% {
+      transform: scale(10);
+      opacity: 0.375;
+    }
+    100% {
+      transform: scale(35);
+      opacity: 0;
+    }
+  }
+`;
+
+export const Content = styled.span`
+  position: relative;
+  z-index: 2;
+  font-size: 0.938rem;
+  font-weight: 500;
+`;
+
+export const LoadingComponent = styled.div<LoadingComponentProps>`
+  ${({ isShow, color }) =>
+    css`
+      color: ${color};
+      position: absolute;
+      inset: 0;
+      visibility: ${isShow ? "visible" : "hidden"};
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      svg {
+        animation: loading 1.5s linear infinite;
+      }
+
+      @keyframes loading {
+        0% {
+          transform: rotateZ(0deg);
+        }
+        100% {
+          transform: rotateZ(-360deg);
+        }
+      }
+    `}
 `;
